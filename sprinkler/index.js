c8665556d361d63a6b55b052bdc1f140949d1b70
sganeshkumaar <sganeshkumaar@solitontech.com>
2023-12-08 11:00:02 +0530

    feat: finish html and css, start js

---
+++ sprinkler/index.js
@@ -0,0 +1,20 @@
+const frontInput =  document.querySelector('#front-radio')
+const backInput =  document.querySelector('#back-radio')
+const leftInput =  document.querySelector('#left-radio')
+const rightInput =  document.querySelector('#right-radio')
+
+const checkbox = document.querySelector('#check-box')
+
+const zoneOrder = {
+    front :1,
+    back: 2,
+    right: 3,
+    left: 4
+}
+
+
+checkbox.addEventListener('click',actionOnCheckBoxClick)
+
+function actionOnCheckBoxClick() {
+    
+}
